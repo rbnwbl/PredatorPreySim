8be37b43dd1196776e7f8ca7da7f43a9ee339887
rbnwbl <reubenweibel1@gmail.com>
2025-02-14 00:29:00 +0000

    added some comments and rain to grass as i forgot lol

--- Grass.java
+++ Grass.java
@@ -2,19 +2,41 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * A simple model of a grass plant.
+ * Grass plants age, reproduce and die.
+ * 
+ * @author Yiun Kim and Reuben Weibel
+ * @version 7.1
+ */
 public class Grass extends Plant
 {
+    // Characteristics shared by all grass plants.
+    // The min age a grass needs to be to breed.
     private static final int BREEDING_AGE = 10;
+    // The age to which a grass plant can live.
     private static final int MAX_AGE = 20;
+    // The likelihood that a grass plant reproduces.
     private static final double BREEDING_PROBABILITY = 0.12;
+    // The max amount of 'children' a grass plant can birth at a time.
     private static final int MAX_YIELD = 8;
+    // The nutritional value of a grass plant.
     private static final int NUTRITION = 1;
+    // The range a grass plant can mate in.
     private static final int MATE_RANGE = 2;
 
+    // The grass' age.
     private int age;
 
+    // A shared random number generator.
     private static final Random rand = Randomizer.getRandom();
 
+    /**
+     * Constructor for instances of Grass.
+     * Assigns a random age, up to the max age, to the plant.
+     * @param randomAge True if the plant should have a random age set, false if age should = 0.
+     * @param location  The plant's location.
+     */
     public Grass(boolean randomAge, Location location)
     {
         super(location,NUTRITION);
@@ -24,6 +46,16 @@ public class Grass extends Plant
         }
     }
 
+    /**
+     * Method that is run at each step of the simulation.
+     * The grass ages and reproduces.
+     * 
+     * @param currentField The field currently occupied.
+     * @param nextFieldState The updated field.
+     * @param time The current time of the simulation.
+     * @param weather The current state of the weather.
+     * 
+     */
     public void act(Field currentField,Field nextFieldState, int time, Weather weather)
     {
         incrementAge();
@@ -36,6 +68,9 @@ public class Grass extends Plant
         }
     }
 
+    /**
+     * Increases the grass' age. Could result in the grass' death.
+     */
     private void incrementAge()
     {
         age++;
@@ -58,12 +93,13 @@ public class Grass extends Plant
      * Check whether or not this grass is to reproduce at this step.
      * New plants will be made into free adjacent locations.
      * @param freeLocations The locations that are free in the current field.
+     * @param weather The current state of the weather.
      */
     private void giveBirth(Field currentField, Field nextFieldState, List<Location> freeLocations, Weather weather)
     {
         // New plants are born into adjacent locations.
         // Get a list of adjacent free locations.
-        int births = breed();
+        int births = breed(weather.getRain());
         if(births > 0 && canMate(currentField,weather.getVisibility())) {
             for (int b = 0; b < births && !freeLocations.isEmpty(); b++) {
                 Location loc = freeLocations.remove(0);
@@ -76,12 +112,18 @@ public class Grass extends Plant
     /**
      * Generate a number representing the yield,
      * if it can breed.
+     * @param isRaining True if it is raining.
      * @return The number of births (may be zero).
      */
-    private int breed()
+    private int breed(boolean isRaining)
     {
         int births;
-        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+        double newBreedingProbability = BREEDING_PROBABILITY;
+        // More likely to reproduce if it is raining.
+        if (isRaining) {
+            newBreedingProbability = newBreedingProbability * 1.5;
+        }
+        if(canBreed() && rand.nextDouble() <= newBreedingProbability) {
             births = rand.nextInt(MAX_YIELD) + 1;
         }
         else {
@@ -101,6 +143,8 @@ public class Grass extends Plant
 
     /**
      * A plant can mate if there is a plant of opposite sex within MATE_RANGE
+     * @param field The field the grass is currently in.
+     * @param visibility The current visibility given from Weather.
      */
     private boolean canMate(Field field,int visibility)
     {
--- Weather.java
+++ Weather.java
@@ -1,19 +1,35 @@
 import java.util.Random;
 
+/**
+ * This is a class that represents the weather in the simulation.
+ * @author Yiun Kim and Reuben Weibel
+ * @version 7.1
+ */
 public class Weather {
-
+    // Characteristics of the weather.
+    // The current temperature.
     private int temp;
+    // The current visibility.
     private int visibility;
+    // Whether it is currently raining.
     private boolean rain;
 
+    // Bounds for the temperature attribute.
     private static final int MIN_TEMP = 20;
     private static final int MAX_TEMP = 30;
 
+    // Bounds for the visibility attribute.
     private static final int MIN_VISIBILITY = -1;
-    private static final int MAX_VISIBILITY = 2;
+    private static final int MAX_VISIBILITY = 1;
     
+    // Random number generator.
     private final static Random rand = Randomizer.getRandom();
     
+    /**
+     * Constructor for instances of the Weather class.
+     * Assigns a random temperature and visibility.
+     * Decides if it is raining or not (0.2 chance of rain).
+     */
     public Weather() {
         temp = rand.nextInt(MIN_TEMP,MAX_TEMP);
         visibility = rand.nextInt(MIN_VISIBILITY, MAX_VISIBILITY);
@@ -21,28 +37,44 @@ public class Weather {
         rain = (rand.nextDouble() < 0.2)?true:false;
     }
 
+    /**
+     * Method that updates the weather.
+     */
     public void change() {
         // Change temp by 1 degree (positively or negatively)
         do {
             temp = temp + rand.nextInt(-1,2);
         } while (temp < MIN_TEMP || temp > MAX_TEMP);
 
-        visibility = rand.nextInt(MIN_VISIBILITY,MAX_VISIBILITY);
+        // New value for visibility. MAX_VISIBILITY + 1 so possible values are -1,0,1.
+        visibility = rand.nextInt(MIN_VISIBILITY,MAX_VISIBILITY +1);
         
-
+        // Update whether it is raining or not (0.2 chance of rain).
         rain = (rand.nextDouble() < 0.2)?true:false;
     }
 
+    /**
+     * Getter method for temperature
+     * @return The current temperature.
+     */
     public int getTemp()
     {
         return temp;
     }
     
+    /**
+     * Getter method for visibility.
+     * @return The current visibility.
+     */
     public int getVisibility()
     {
         return visibility;
     }
 
+    /**
+     * Getter method for rain.
+     * @return True if it is currently raining.
+     */
     public boolean getRain() 
     {
         return rain;
